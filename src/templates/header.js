c4bf56d333e56b4d4cdc54f207ea40752b82cb40
Gerardo Ramirez <gerardogo145@gmail.com>
2021-04-22 14:10:02 +0200

    :art: [ADD] Template and page of character

---
+++ src/templates/header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    const view = `
+        <div class="header-main">
+            <div class="header-logo">
+                <h1><a href="/">100tifi.co</a></h1>
+            </div>
+            <div class="header-nav">
+                <a hre="#/about/">About</a>
+            </div>
+        </div>
+    `;
+    return view;
+};
+
+export default Header;
