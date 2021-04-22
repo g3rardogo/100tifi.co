bffa62905593d35bf8e2e2af20c9011d6eb24438
Gerardo Ramirez <gerardogo145@gmail.com>
2021-04-22 12:54:42 +0200

    :wrench: [ADD] Webpack config file and .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+//Acceder a la variable path que nos permitira saber en que directorio nos encontramos y hacia donde nos moveremos
+const path = require('path');
+//Asignando el plugin de HTML Webpack a una variable
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//Modulo que exporta la configuracion
+module.export = {
+    //Punto de entrada
+    entry: './src/index.js',
+    //Ruta a la que enviaremos el codigo listo y preparado para produccion
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        //Nombre del archivo ya compilado
+        filename: 'main.js'
+    },
+    //Trabajando sobre las extensiones
+    resolve: {
+        extensions: ['.js'],
+    },
+    //Modulo de las reglas
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                //Excluyendo la carpeta node_modules
+                exclude: /node_modules/,
+                //Indicamos que utilizaremos el babel loader
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    //Seccion de plugins
+    plugins: [
+        new HtmlWebpackPlugin([
+            {
+                //Indicamos que vamos a injectar un valor al archivo HTML
+                inject: true,
+                //Ruta de donde se encuentra el template principal
+                template: './public/index.html',
+                //Nombre y ruta que tendra el archivo final, en este caso partimos de la carpeta dist
+                filename: './index.html'
+            }
+        ])
+    ]
+}
